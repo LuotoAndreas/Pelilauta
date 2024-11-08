237f4b86300a036d6c58b6b3a09e227b19c2eff5
Andreas Luoto <luoto.andreas@gmail.com>
2024-11-08 15:30:47 +0200

    toimiva tason1 ohjelma

--- vt2.py
+++ vt2.py
@@ -1,12 +1,46 @@
-#Toteuta sovellus tähän tiedostoon
-# -*- coding: utf-8 -*-
-from flask import Flask, request, Response
-import os
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
-# @app.route määrää mille osoitteille tämä funktio suoritetaan
-@app.route('/')
-def hello_world():
-    return Response("Hello World!", content_type="text/plain; charset=UTF-8")
-    
+# template-tiedostojen automaattinen lataus
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
+# Asetetaan ruudukon minimikoko ja maksimikoko
+MIN_SIZE = 8
+MAX_SIZE = 16
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    # Alustetaan oletusarvot
+    board_size = MIN_SIZE
+    player1 = "Pelaaja 1"
+    player2 = "Pelaaja 2"
+    error_message = ""
+
+    if request.method == "POST":
+        # Otetaan käyttäjän syötteet talteen        
+        size_input = request.form.get("size", str(MIN_SIZE))
+
+        try:
+            player1 = request.form.get("player1", "Pelaaja 1")
+            player2 = request.form.get("player2", "Pelaaja 2")
+
+            if player1 == "" or player2 == "":
+                raise ValueError("Nimi ei ole sallittu.")
+        except ValueError:
+            error_message = "Molempien pelaajien nimet pitää syöttää."
+
+
+
+        try:
+            board_size = int(size_input)
+            if board_size < MIN_SIZE or board_size > MAX_SIZE:
+                raise ValueError("Koko ei ole sallittu.")
+        except ValueError:
+            board_size = MIN_SIZE
+            error_message = "Syöttämäsi arvo ei kelpaa."
+
+    return render_template("pohja.xhtml", board_size = board_size, player1 = player1, player2 = player2, error_message = error_message)
+
+if __name__ == "__main__":
+    app.run(debug=True)
